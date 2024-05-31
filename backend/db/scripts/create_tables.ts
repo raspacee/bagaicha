bc476bedb2c8285354af0551710a2ee28929a2a4
Bijay Khapung <khapungbj84@gmail.com>
2024-05-31 16:25:31 +0545

    Add tables creation functions

--- backend/db/scripts/create_tables.ts
+++ backend/db/scripts/create_tables.ts
@@ -14,6 +14,11 @@ async function main() {
     await create_place(client);
     await create_review(client);
     await create_like(client);
+    await create_comment(client);
+    await create_notification(client);
+    await create_place_review(client);
+    await create_bookmark(client);
+    await create_search(client);
     client.query("end transaction");
     console.log("Transaction ended");
   } catch (err) {
@@ -26,10 +31,62 @@ async function main() {
   }
 }
 
+async function create_comment(client: PoolClient) {
+  const file = fs.readFileSync(
+    path.join(__dirname, "..", "tables", "comment.sql"),
+    "utf-8",
+  );
+  await client.query(file);
+  console.log("Comment table created");
+
+  const file2 = fs.readFileSync(
+    path.join(__dirname, "..", "tables", "comment_like.sql"),
+    "utf-8",
+  );
+  await client.query(file2);
+  console.log("Comment like table created");
+}
+
+async function create_notification(client: PoolClient) {
+  const file = fs.readFileSync(
+    path.join(__dirname, "..", "tables", "notification.sql"),
+    "utf-8",
+  );
+  await client.query(file);
+  console.log("Notification table created");
+}
+
+async function create_bookmark(client: PoolClient) {
+  const file = fs.readFileSync(
+    path.join(__dirname, "..", "tables", "review_bookmark.sql"),
+    "utf-8",
+  );
+  await client.query(file);
+  console.log("Bookmark table created");
+}
+
+async function create_search(client: PoolClient) {
+  const file = fs.readFileSync(
+    path.join(__dirname, "..", "tables", "search.sql"),
+    "utf-8",
+  );
+  await client.query(file);
+  console.log("Search table created");
+}
+
+async function create_place_review(client: PoolClient) {
+  const file = fs.readFileSync(
+    path.join(__dirname, "..", "tables", "place_review.sql"),
+    "utf-8",
+  );
+  await client.query(file);
+  console.log("Place review table created");
+}
+
 async function create_user(client: PoolClient) {
   const file = fs.readFileSync(
     path.join(__dirname, "..", "tables", "user.sql"),
-    "utf-8"
+    "utf-8",
   );
   await client.query(file);
   console.log("User table created");
@@ -38,7 +95,7 @@ async function create_user(client: PoolClient) {
 async function create_place(client: PoolClient) {
   const file = fs.readFileSync(
     path.join(__dirname, "..", "tables", "place.sql"),
-    "utf-8"
+    "utf-8",
   );
   await client.query(file);
   console.log("Place table created");
@@ -47,7 +104,7 @@ async function create_place(client: PoolClient) {
 async function create_review(client: PoolClient) {
   const file = fs.readFileSync(
     path.join(__dirname, "..", "tables", "review.sql"),
-    "utf-8"
+    "utf-8",
   );
   await client.query(file);
   console.log("Review table created");
@@ -56,7 +113,7 @@ async function create_review(client: PoolClient) {
 async function create_like(client: PoolClient) {
   const file = fs.readFileSync(
     path.join(__dirname, "..", "tables", "like.sql"),
-    "utf-8"
+    "utf-8",
   );
   await client.query(file);
   console.log("Like table created");
