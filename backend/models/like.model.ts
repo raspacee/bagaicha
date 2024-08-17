ded7571328ad2109f5e5052dae2e130d3b2753e5
Bijay Khapung <khapungbj84@gmail.com>
2024-08-17 14:50:38 +0545

    Fix like/unlike not incrementing likeCount

--- backend/models/like.model.ts
+++ backend/models/like.model.ts
@@ -1,23 +1,57 @@
 import { pool } from "../db/index";
-import { v4 as uuidv4 } from "uuid";
 
 const createPostLike = async (postId: string, userId: string) => {
-  const text = `
-  INSERT INTO "postLike" ("postId", "likerId")
-  VALUES ($1, $2);
-`;
-  const values = [postId, userId];
-  await pool.query(text, values);
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    const text = `
+    INSERT INTO "postLike" ("postId", "likerId")
+    VALUES ($1, $2);
+    `;
+    const values = [postId, userId];
+    await client.query(text, values);
+
+    const text2 = `
+    UPDATE "post"
+      SET "likeCount" = "likeCount" + 1
+    WHERE "id" = $1`;
+    const values2 = [postId];
+    await client.query(text2, values2);
+
+    await client.query("COMMIT");
+  } catch (err) {
+    console.log(err);
+    await client.query("ROLLBACK");
+    throw new Error();
+  }
 };
 
 const deletePostLike = async (postId: string, userId: string) => {
-  const text = `
-  DELETE FROM "postLike"
-  WHERE "postId" = $1
-    AND "likerId" = $2;
-`;
-  const values = [postId, userId];
-  await pool.query(text, values);
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+
+    const text = `
+    DELETE FROM "postLike"
+      WHERE "postId" = $1
+      AND "likerId" = $2;
+    `;
+    const values = [postId, userId];
+    await client.query(text, values);
+
+    const text2 = `
+    UPDATE "post"
+      SET "likeCount" = "likeCount" - 1
+    WHERE "id" = $1`;
+    const values2 = [postId];
+    await client.query(text2, values2);
+
+    await client.query("COMMIT");
+  } catch (err) {
+    console.log(err);
+    await client.query("ROLLBACK");
+    throw new Error();
+  }
 };
 
 const exporter = {
