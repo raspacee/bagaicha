496287717696a5e32e4da34fbbed413b439e1c5b
Bijay Khapung <khapungbj84@gmail.com>
2024-10-09 11:58:02 +0545

    Add production db config

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -1,10 +1,8 @@
 import { Pool, QueryConfigValues } from "pg";
 
-const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined;
-
-const pool = new Pool({
+const devConfig = {
   host: process.env.DB_HOST,
-  port: port,
+  port: parseInt(process.env.DB_PORT || "5432"),
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
@@ -14,7 +12,25 @@ const pool = new Pool({
   max: 50,
   idleTimeoutMillis: 10000,
   connectionTimeoutMillis: 4000,
-});
+};
+
+const prodConfig = {
+  host: process.env.DB_HOST_PROD,
+  port: parseInt(process.env.DB_PORT_PROD || "5432"),
+  user: process.env.DB_USER_PROD,
+  database: process.env.DB_NAME_PROD,
+  password: process.env.DB_PASSWORD_PROD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  max: 50,
+  idleTimeoutMillis: 10000,
+  connectionTimeoutMillis: 4000,
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV == "development" ? devConfig : prodConfig
+);
 
 const query = async <I = any[]>(text: string, params: QueryConfigValues<I>) => {
   return pool.query(text, params);
